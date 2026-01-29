ee739dd6cb50e0c5b0e28d91e573287f0a6ac6f8
shubhamkumar-dianapps <shubham.kumar@dianapps.com>
2026-01-29 17:34:12 +0530

    Stop tracking sensitive modules (orders, cart, shipping, payments)

--- ecommerce/apps/orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from apps.orders.views.order import OrderViewSet
-
-router = DefaultRouter()
-router.register(r"", OrderViewSet, basename="order")
-
-urlpatterns = router.urls
